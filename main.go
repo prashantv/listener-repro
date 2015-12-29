bd39b3482e732398c7c1a1b840345b28a6fd4f20
Prashant Varanasi <prashant@uber.com>
2015-12-28 21:13:13 -0800

    Add simple repro for listener

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+const (
+	connectionsBeforeClose = 5
+	testIterations         = 1000
+	enableSleep            = false
+)
+
+func runTest() error {
+	addr, ln := startListener()
+
+	for i := 0; i < connectionsBeforeClose; i++ {
+		if err := connect(addr); err != nil {
+			panic(err)
+		}
+	}
+	ln.Close()
+
+	if enableSleep {
+		time.Sleep(time.Millisecond)
+	}
+
+	if err := connect(addr); err == nil {
+		return fmt.Errorf("connect succeeded even though it shouldn't have")
+	}
+	return nil
+}
+
+func main() {
+	for i := 0; i < testIterations; i++ {
+		if err := runTest(); err != nil {
+			fmt.Println("Iteration", i, "failed with", err)
+		}
+	}
+}
+
+func startListener() (string, net.Listener) {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
+	}
+	go acceptLoop(ln)
+
+	addr := ln.Addr().String()
+	fmt.Println("TCP Listener started on", addr)
+	return addr, ln
+}
+
+func connect(addr string) error {
+	conn, err := net.Dial("tcp", addr)
+	if err == nil {
+		conn.Close()
+	}
+	return err
+}
+
+func acceptLoop(ln net.Listener) {
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			return
+		}
+
+		conn.Close()
+	}
+}
